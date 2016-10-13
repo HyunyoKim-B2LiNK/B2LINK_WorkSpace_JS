6f0226e03b79bae4a9e83ed958bdfde5d9cb4fdf
thdtjsdn <thdtjsdn@gmail.com>
2016-10-13 19:14:13 +0900

    Revert "[ S ] DataBase"
    This reverts commit 27770a3f3fbbd1cff6738e83441c6281273282e5.

--- DataBase/3.2.10_x64/MongoDB Start 0 - B2LINK.bat
+++
@@ -1,3 +0,0 @@
-del /q E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\Log\B2LINK\0\.
-E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\bin\mongod --config E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\Config\B2LINK\0.conf
-:start /MIN /REALTIME E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\bin\mongod --config E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\Config\B2LINK\0.conf
