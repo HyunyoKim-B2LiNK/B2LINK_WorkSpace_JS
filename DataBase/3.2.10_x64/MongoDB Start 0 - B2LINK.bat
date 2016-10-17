ec7548c5e3f69b50cbd98ed2333c565f060f2920
ray park <raybirdy@gmail.com>
2016-10-17 18:47:09 +0900

    경로 수정
    상대경로로 반영

--- DataBase/3.2.10_x64/MongoDB Start 0 - B2LINK.bat
+++ DataBase/3.2.10_x64/MongoDB Start 0 - B2LINK.bat
@@ -1,3 +1,2 @@
-del /q E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\Log\B2LINK\0\.
-E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\bin\mongod --config E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\Config\B2LINK\0.conf
-:start /MIN /REALTIME E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\bin\mongod --config E:\WorkSpace_B2LINK\DataBase\3.2.10_x64\Config\B2LINK\0.conf
+del /q DataBase\3.2.10_x64\Log\B2LINK\0\.
+DataBase\3.2.10_x64\bin\mongod --config DataBase\3.2.10_x64\Config\B2LINK\0.conf
