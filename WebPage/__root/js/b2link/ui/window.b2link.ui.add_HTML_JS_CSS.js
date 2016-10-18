ac49c69aa67eea679c4d7ebe8e9f27a5365c8b0b
thdtjsdn <thdtjsdn@gmail.com>
2016-10-18 11:55:16 +0900

    add HTML JS CSS 공통 소스 WRAPPING API
    add HTML JS CSS 공통 소스 WRAPPING API

---
+++ WebPage/__root/js/b2link/ui/window.b2link.ui.add_HTML_JS_CSS.js
@@ -0,0 +1,37 @@
+//----------------------------------------------------------------------------------------------------;
+var fileNm = "js/b2link/ui/window.b2link.ui.add_HTML_JS_CSS.js";
+if( console ) console.log( "[ S ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
+
+/**
+ * @function
+ * @param {Object} d data
+ * <code>
+ * {
+ *	"nm" : ""
+ *	, "url" : ""
+ *	, "css_url : ""
+ *	, "css_front_nm : ""
+ * }
+ * </code>
+ * @return {Array} [ {HTMLElement}, {Object} ]
+ */
+window.b2link.ui.add_HTML_JS_CSS = function( d )
+{
+	window.TtwLog.timeStamp( "---- [ S ] - window.b2link.ui.add_HTML_JS_CSS():{Array}----------" );
+
+	var html = window.b2link.ui.__getFileNM_HTML( d.url, d.nm );
+	var js = window.b2link.ui.__getFileNM_JS( d.url, d.nm );
+	var css = window.b2link.ui.__getFileNM_CSS( d.css_url, d.css_front_nm + d.nm );
+
+	var f = window.b2link.util.importHTMLJSCSS__And_Add_b2link_STATIC_UI;
+	var o = f( d.nm, html, js, css, window.b2link.STATIC.CONST.ROOT_DIV );
+
+	window.TtwLog.timeStamp( "---- [ E ] - window.b2link.ui.add_HTML_JS_CSS():{Array}----------" );
+
+	return o;
+};
+
+//----------------------------------------------------------------------------------------------------;
+if( console ) console.log( "[ E ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
---
+++ WebPage/__root/js/b2link/ui/window.b2link.ui.add_HTML_JS_CSS__CheckStaticUI.js
@@ -0,0 +1,43 @@
+//----------------------------------------------------------------------------------------------------;
+var fileNm = "js/b2link/ui/window.b2link.ui.add_HTML_JS_CSS__CheckStaticUI.js";
+if( console ) console.log( "[ S ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
+
+/**
+ * @function
+ * @param {Object} d data
+ * <code>
+ * {
+ *	"nm" : ""
+ *	, "url" : ""
+ *	, "css_url : ""
+ *	, "css_front_nm : ""
+ * }
+ * </code>
+ * @return {Array} [ {HTMLElement}, {Object} ]
+ */
+window.b2link.ui.add_HTML_JS_CSS__CheckStaticUI = function( d )
+{
+	window.TtwLog.timeStamp( "---- [ S ] - window.b2link.ui.add_HTML_JS_CSS__CheckStaticUI():{Array}----------" );
+
+	if( window.b2link.STATIC.UI.__checkKey( d.nm ) )
+	{
+		window.b2link.STATIC.UI.__show( d.nm );
+		return window.b2link.STATIC.UI.__getObj( d.nm );
+	}
+
+	var html = window.b2link.ui.__getFileNM_HTML( d.url, d.nm );
+	var js = window.b2link.ui.__getFileNM_JS( d.url, d.nm );
+	var css = window.b2link.ui.__getFileNM_CSS( d.css_url, d.css_front_nm + d.nm );
+
+	var f = window.b2link.util.importHTMLJSCSS__And_Add_b2link_STATIC_UI;
+	var o = f( d.nm, html, js, css, window.b2link.STATIC.CONST.ROOT_DIV );
+
+	window.TtwLog.timeStamp( "---- [ E ] - window.b2link.ui.add_HTML_JS_CSS__CheckStaticUI():{Array}----------" );
+
+	return o;
+};
+
+//----------------------------------------------------------------------------------------------------;
+if( console ) console.log( "[ E ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
