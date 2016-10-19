da86e6502072b932f928e5f1d6f0289bcbba8451
thdtjsdn <thdtjsdn@gmail.com>
2016-10-19 10:59:12 +0900

    [ N ] DataBase Schema - list_static - email
    [ N ] DataBase Schema - list_static - email

---
+++ DataBase_Schema/B2LINK/database-insert/list_static/email/0.js
@@ -0,0 +1,23 @@
+/*/
+{
+	TLD : "com"//Top Level Domain;
+	, ccTLD : ""//Country Code Top Level Domain - kr, us, jp;
+	, gTLD : "com"//Generic Top Level Domain - com, net, org;
+	, SLD : "gmail"//Second Level Domain;
+}
+//*/
+var a = [
+	  { SLD : "daum" , TLD : "net", ccTLD : "", gTLD : "net" }
+	, { SLD : "gmail", TLD : "com", ccTLD : "", gTLD : "com" }
+	, { SLD : "naver", TLD : "com", ccTLD : "", gTLD : "com" }
+];
+//----------------------------------------------------------------------------------------------------;
+var db0 = db.getSiblingDB( "list_static" );
+var io;
+var i=0, iLen=a.length;
+for( ; i<iLen; ++i ){
+	var io = a[ i ];
+		io._id = NumberInt( i );
+		io.cd_n = NumberInt( io.cd_n );
+	db0.email.insert( io );
+};
