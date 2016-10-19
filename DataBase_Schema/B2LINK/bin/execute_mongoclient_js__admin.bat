e20333e0192a21984eeee0aa2fb476ebdec0f594
thdtjsdn <thdtjsdn@gmail.com>
2016-10-19 12:09:24 +0900

    [ U ] DataBase Bat 경로 수정
    [ U ] DataBase Bat 경로 수정

--- DataBase_Schema/B2LINK/bin/execute_mongoclient_js__admin.bat
+++ DataBase_Schema/B2LINK/bin/execute_mongoclient_js__admin.bat
@@ -1,4 +1,4 @@
-SET _MONGO=..\..\..\DataBase\3.2.10_x64\bin\mongo
+SET _MONGO=..\..\..\MongoDB\3.2.10_x64\bin\mongo
 SET   _URL=127.0.0.1:59320
 
 %_MONGO% %_URL%/admin -u Ttw -p thdtjsdncjswoQWEASDZXC %1
--- DataBase_Schema/B2LINK/bin/execute_mongoclient_js__database.bat
+++ DataBase_Schema/B2LINK/bin/execute_mongoclient_js__database.bat
@@ -1,4 +1,4 @@
-SET _MONGO=..\..\..\DataBase\3.2.10_x64\bin\mongo
+SET _MONGO=..\..\..\MongoDB\3.2.10_x64\bin\mongo
 SET   _URL=127.0.0.1:59320
 
 %_MONGO% %_URL%/%1 -u %2 -p %3 %4
