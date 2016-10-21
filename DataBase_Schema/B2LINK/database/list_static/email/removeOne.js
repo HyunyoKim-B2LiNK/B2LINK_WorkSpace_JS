abd2fe04b4bef87f01bd1cee2ef5ff6d345407fc
ray park <raybirdy@gmail.com>
2016-10-21 19:58:41 +0900

    Revert "[ DATABSE"
    This reverts commit ab626a0a1f2922bba2daf75359bf9e9e69d09770.

--- DataBase_Schema/B2LINK/database/list_static/email/removeOne.js
+++
@@ -1,9 +0,0 @@
-function( d )
-{
-	print( "-- [ S ] - email$removeOne():void----------" );
-
-	print( "email$_getCol() - " + email$_getCol() );
-
-	print( "-- [ E ] - email$removeOne():void----------" );
-	email$getCol().remove( d , 1 );
-}
