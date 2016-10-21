701abeed23fa46c04c3117d832acebdb545440d7
thdtjsdn <thdtjsdn@gmail.com>
2016-10-21 13:04:12 +0900

    [ Database Schema ] - D - delete error log
    [ Database Schema ] - D - delete error log

--- DataBase_Schema/B2LINK/database/list_static/email/checkEmail__overlap.js
+++ DataBase_Schema/B2LINK/database/list_static/email/checkEmail__overlap.js
@@ -1,28 +1,35 @@
 function( d )
 {
-	print( "-- [ S ] - email$checkEmail_overlap():void----------" );
+	print( "-- [ S ] - email$checkEmail_overlap():{uint}----------" );
 	/*/
 	필수 필드
 	SLD, c
+
 	다음중 하나는 무조건 들어가야 함.
 	TLD, ccTLD, gTLD
 	//*/
-	// 소문자로 변경
-	d.sld = d.sld.toLowerCase();
-	d.tld = d.tld.toLowerCase();
-	d.cctld = d.cctld.toLowerCase();
-	d.gtld = d.gtld.toLowerCase();
+
+	//소문자로 변경;
+	d.SLD = d.sld.toLowerCase();
+	d.TLD = d.tld.toLowerCase();
+	d.ccTLD = d.cctld.toLowerCase();
+	d.gTLD = d.gtld.toLowerCase();
 	d.c = d.c.toLowerCase();
 
-
 	var r = email$findOne({
-	 SLD : d.sld
-	 , TLD : d.tld
-	 , ccTLD : d.cctld
-	 , gTLD : d.gtld
-	 , c : d.c
+		SLD : d.SLD
+		, TLD : d.TLD
+		, ccTLD : d.ccTLD
+		, gTLD : d.gTLD
+		, c : d.c
  	});
-	if ( null == r ) return { result : 0 };
 
+	if( null == r )
+	{
+		print( "-- [ E ] - email$checkEmail_overlap():{uint}----------return { result : 1 };" );
+		return { result : 0 };
+	}
+
+	print( "-- [ E ] - email$checkEmail_overlap():{uint}----------return { result : 1 };" );
 	return { result : 1 };
 }
--- DataBase_Schema/B2LINK/database/list_static/email/validation__addDoc.js
+++ DataBase_Schema/B2LINK/database/list_static/email/validation__addDoc.js
@@ -7,13 +7,12 @@ function( d )
 
 	다음중 하나는 무조건 들어가야 함.
 	TLD, ccTLD, gTLD
-
 	//*/
-	// 소문자로 변경
-	d.sld = d.sld.toLowerCase();
-	d.tld = d.tld.toLowerCase();
-	d.cctld = d.cctld.toLowerCase();
-	d.gtld = d.gtld.toLowerCase();
+	//소문자로 변경;
+	d.SLD = d.SLD.toLowerCase();
+	d.TLD = d.TLD.toLowerCase();
+	d.ccTLD = d.ccTLD.toLowerCase();
+	d.gTLD = d.gTLD.toLowerCase();
 	d.c = d.c.toLowerCase();
 	print( "-- [ E ] - email$validation__addDoc():void----------" );
 	return d;
