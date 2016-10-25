fe1f7fe27ad189987fbec9f1255d410ec8ce7f6d
thdtjsdn <thdtjsdn@gmail.com>
2016-10-25 18:48:02 +0900

    [ WebPage ] - U - list_static.country - add UI에서 데이터 추가 후 리스트로 바뀌게
    [ WebPage ] - U - list_static.country - add UI에서 데이터 추가 후 리스트로 바뀌게

--- WebPage/root/js/b2link_ui/country/window.b2link_ui.country.add.js
+++ WebPage/root/js/b2link_ui/country/window.b2link_ui.country.add.js
@@ -20,6 +20,25 @@ window.b2link_ui.country.add = function()
 
 	//var o = window.b2link.ui.add_HTML_JS_CSS( d );
 	var o = window.b2link.ui.add_HTML_JS_CSS__CheckStaticUI( d );
+	if( !o[ 1 ].getEvt_Complete__Add() )
+	{
+		o[ 1 ].setEvt_Complete__Add( function( result ){
+			window.TtwLog.timeStamp( "[ S ] - o[ 1 ].setEvt_Complete__Add( function( result ){----------" );
+			if( window.b2link.fn.getResultStatus( result ) )
+			{
+				//*/
+				o[ 1 ].hide();
+				/*/
+				window.b2link.element.setPosition_RightTop( o[ 0 ], 10, 300 );
+				//*/
+
+				window.b2link_ui.country.getAllList();
+			}
+			else alert( "등록 실패." );
+
+			window.TtwLog.timeStamp( "[ E ] - o[ 1 ].setEvt_Complete__Add( function( result ){----------" );
+		});
+	}
 
 	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_ui.country.add():void----------" );
 
