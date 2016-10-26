bcd3a6b24331da47dbdb246af869686c839acf16
thdtjsdn <thdtjsdn@gmail.com>
2016-10-26 21:16:28 +0900

    ㅁㄴㅇ
    ㅁㄴㅇ

--- DataBase_Schema/B2LINK/database-insert/brand/brand_basic/0.js
+++ DataBase_Schema/B2LINK/database-insert/brand/brand_basic/0.js
@@ -1,10 +1,21 @@
-var a = [
-	  { "cd_brand" : "" , "nm_kr" : "파파레서피", "nm_cn" : "爸爸的礼物", "nm_us" : "papa recipe", "url" : "http://paparecipe.com/", "logo_url" : "", "trademark_url" : "", "register_date" : "" , "register_person" : ""  , "description" : "" } ];
+var a = [{
+	"nm_cn" : "爸爸的礼物",
+	"description" : "",
+	"nm_kr" : "파파레서피",
+	"logo_url" : "",
+	"url" : "http://paparecipe.com/",
+	"register_date" : "",
+	"trademark_url" : "",
+	"cd_brand" : "",
+	"nm_us" : "papa recipe",
+	"register_person" : ""
+}];
 //----------------------------------------------------------------------------------------------------;
 var db0 = db.getSiblingDB( "brand" );
 var io;
 var i=0, iLen=a.length;
-for( ; i<iLen; ++i ){
+for( ; i<iLen; ++i )
+{
 	var io = a[ i ];
 		io._id = NumberInt( i );
 	db0.brand_basic.insert( io );
