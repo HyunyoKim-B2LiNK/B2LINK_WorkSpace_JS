24e7ff3f3daff2313d422d72e2adb03b45db795a
thdtjsdn <thdtjsdn@gmail.com>
2016-10-26 19:09:25 +0900

    [ Database Schema ] - U - list_static.email_detail - _id 수치 예외처리
    [ Database Schema ] - U - list_static.email_detail - _id 수치 예외처리

--- DataBase_Schema/B2LINK/database/list_static/email_detail/remove_id.js
+++ DataBase_Schema/B2LINK/database/list_static/email_detail/remove_id.js
@@ -3,6 +3,9 @@ function( _id )
 	print( "-- [ S ] - email_detail$remove_id():{uint}----------" );
 	print( "_id : " + _id );
 	printjson( _id );
+
+	if( !( _id > -1 ) ) return 0;
+
 	var r = email_detail$_getCol().remove( { _id : NumberInt( _id ) }, 1 );
 	print( r );
 	print( "-- [ E ] - email_detail$remove_id():{uint}----------" );
