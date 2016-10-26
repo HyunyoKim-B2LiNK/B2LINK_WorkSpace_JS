f1e00cca0c9429fe0c798a06b28100aec94bb49b
thdtjsdn <thdtjsdn@gmail.com>
2016-10-26 16:12:28 +0900

    [ HTTPServer_MongoDB ] - U - 소스 정리
    [ HTTPServer_MongoDB ] - U - 소스 정리

--- HTTPServer_MongoDB/MemberSession/js/b2link_router/list_static/email_detail/remove_id.js
+++ HTTPServer_MongoDB/MemberSession/js/b2link_router/list_static/email_detail/remove_id.js
@@ -12,9 +12,6 @@ http://localhost:49320/list_static/email_detail/remove_id
 	var t = global.server;//HTTP Server;
 	var q = global.b2link.url.getQueryFromURL( req.url );//Query;
 
-	//use q;
-	//...Custom parameter process;
-
 	global.b2link.request.get__member_session$checkSessionAndReqMongoDB( req, res, q, "list_static" , "email_detail$remove_id(" + q._id + ")");
 
 	global.RayLog.log( "- [ E ] - email_detail/remove_id():void----------" );
--- HTTPServer_MongoDB/MemberSession/js/b2link_router/list_static/email_detail/remove_ids.js
+++ HTTPServer_MongoDB/MemberSession/js/b2link_router/list_static/email_detail/remove_ids.js
@@ -12,9 +12,6 @@ http://localhost:49320/list_static/email_detail/remove_ids
 	var t = global.server;//HTTP Server;
 	var q = global.b2link.url.getQueryFromURL( req.url );//Query;
 
-	//use q;
-	//...Custom parameter process;
-
 	global.b2link.request.get__member_session$checkSessionAndReqMongoDB( req, res, q, "list_static" , "email_detail$remove_ids(" + q.ids + ")");
 
 	global.RayLog.log( "- [ E ] - email_detail/remove_ids():void----------" );
