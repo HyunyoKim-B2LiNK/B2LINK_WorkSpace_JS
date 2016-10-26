6a2656bf6f98c7659643e1fe342286fe2e826451
thdtjsdn <thdtjsdn@gmail.com>
2016-10-26 18:45:51 +0900

    [ HTTPServer_MongoDB ] - U - req 경량화 API로 변경
    [ HTTPServer_MongoDB ] - U - req 경량화 API로 변경

--- HTTPServer_MongoDB/MemberSession/js/b2link_router/member/member_session_log/getAllList.js
+++ HTTPServer_MongoDB/MemberSession/js/b2link_router/member/member_session_log/getAllList.js
@@ -7,15 +7,11 @@ if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 http://localhost:49320/member_session_log/getAllList
 //*/
 (function( req, res ){
-	console.log( "- [ S ] - member_session_log/getAllList():void----------" );
+	global.TtwLog.log( "- [ S ] - member_session_log/getAllList():void----------" );
 
-	var t = global.server;//HTTP Server;
 	var q = global.b2link.url.getQueryFromURL( req.url );//Query;
 
-	//use q;
-	//...Custom parameter process;
+	global.b2link_router.member.req__fNm( req, res, q, "member_session_log$getAllList" );
 
-	global.b2link.request.get__member_session$checkSessionAndReqMongoDB( req, res, q, "member", "member_session_log$getAllList()" );
-
-	console.log( "- [ E ] - member_session_log/getAllList():void----------" );
+	global.TtwLog.log( "- [ E ] - member_session_log/getAllList():void----------" );
 });
