3891bf87edc9a435ef632a9b9a70c4f76e080a6f
thdtjsdn <thdtjsdn@gmail.com>
2016-10-27 14:04:36 +0900

    [ DataBase Schema ] - U - brand.brand_basic - Schema 수정
    [ DataBase Schema ] - U - brand.brand_basic - Schema 수정

--- DataBase_Schema/B2LINK/database-insert/_schema/brand/brand_basic.js
+++ DataBase_Schema/B2LINK/database-insert/_schema/brand/brand_basic.js
@@ -1,16 +1,22 @@
 var d = {
-	 "cd_brand" : "string"
-	, "nm_kr" : "string"
-	, "nm_cn" : "string"
-	, "nm_us" : "string"
-	, "url" : "string"
-	, "logo_url" : "string"
-	, "trademark_url": "string"
-	, "register_date" : "string"
-	, "register_person" : "string"
-	, "description" : "string"
+	"_id" : "brand_basic"
+
+	, "_id$member_basic" : "uint"//{uint} - _id$member_basic;
+
+	, "description" : "string"//{String} - description;
+
+	, "nm_kr" : "string"//{String} - name_kr;
+	, "nm_cn" : "string"//{String} - name_cn;
+	, "nm_us" : "string"//{String} - name_us;
+
+	//, "date_register" : "object"//{Object} - date_register - { "y" : 2016, "m" : 10, "d" : 27, "ho" : 12, "mi" : 20, "se" : 30 };
+	, "date_register" : "array"//{Array} - date_register - [ 2016, 10, 27, 12, 20, 30 ];
+
+	, "url" : "string"//{String} - url - www.b2link.co.kr;
+	, "url_logo" : "string"//{String} - url_logo;
+	, "url_trademark": "string"//{String} - url_trademark;
 };
 
 //----------------------------------------------------------------------------------------------------;
 
-db.getSiblingDB( "_schema" ).brand.brand_basic.insert( d );
+db.getSiblingDB( "_schema" ).brand.insert( d );
