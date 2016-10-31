7c45aa68d6533ff4d0bcf73d757a810b6771b103
thdtjsdn <thdtjsdn@gmail.com>
2016-10-31 18:54:32 +0900

    [ HTTPServer_MongoDB ] - U - member.member_basic - remove_id 수정
    [ HTTPServer_MongoDB ] - U - member.member_basic - remove_id 수정

--- HTTPServer_MongoDB/MemberSession/js/b2link_router/member/member_basic/remove_id.js
+++ HTTPServer_MongoDB/MemberSession/js/b2link_router/member/member_basic/remove_id.js
@@ -11,7 +11,7 @@ http://localhost:49320/list_static/member_basic/remove_id
 
 	var q = global.b2link.url.getQueryFromURL( req.url );//Query;
 
-	global.b2link_validation.member_basic$remove_id( q );
+	global.b2link_validation.member.member_basic$remove_id( q );
 
 	global.b2link_router.member.req__value( req, res, q, "member_basic$remove_id", q._id );
 
