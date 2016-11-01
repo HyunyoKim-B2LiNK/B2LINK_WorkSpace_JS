397d79aaec12d2d3d6c156a568ec41097b77a5d8
ray park <raybirdy@gmail.com>
2016-11-01 18:17:39 +0900

    [ DataBase Schema ] - N - brand - brand_basic remove 추가

---
+++ DataBase_Schema/B2LINK/database/brand/brand_basic/validation__remove_id.js
@@ -0,0 +1,13 @@
+function( n )
+{
+	print( "-- [ S ] - brand_basic$validation__remove_id():{uint}----------" );
+
+	if( n > -1 )
+	{
+		print( "-- [ E ] - brand_basic$validation__remove_id():{uint}----------return 1;" );
+		return 1;
+	}
+
+	print( "-- [ E ] - brand_basic$validation__remove_id():{uint}----------return 0;" );
+	return 0;
+}
