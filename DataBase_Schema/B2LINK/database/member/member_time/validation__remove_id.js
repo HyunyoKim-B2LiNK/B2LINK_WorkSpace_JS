b5450822fd2dc09c8a1fc1960430dc080073b53d
thdtjsdn <thdtjsdn@gmail.com>
2016-11-02 14:14:28 +0900

    [ Database Schema ] - U - member.member_time - 수정
    [ Database Schema ] - U - member.member_time - 수정

---
+++ DataBase_Schema/B2LINK/database/member/member_time/validation__remove_id.js
@@ -0,0 +1,13 @@
+function( n )
+{
+	print( "-- [ S ] - member_time$validation__remove_id():{uint}----------" );
+
+	if( n > -1 )
+	{
+		print( "-- [ E ] - member_time$validation__remove_id():{uint}----------return 1;" );
+		return 1;
+	}
+
+	print( "-- [ E ] - member_time$validation__remove_id():{uint}----------return 0;" );
+	return 0;
+}
