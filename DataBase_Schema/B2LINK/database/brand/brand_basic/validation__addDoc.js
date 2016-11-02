304fe5403f2c24624ba477eb46823a164e3509d6
thdtjsdn <thdtjsdn@gmail.com>
2016-11-02 16:53:17 +0900

    [ Database Schema ] - U - brand.brand_basic - 소스 정리
    [ Database Schema ] - U - brand.brand_basic - 소스 정리

--- DataBase_Schema/B2LINK/database/brand/brand_basic/validation__addDoc.js
+++ DataBase_Schema/B2LINK/database/brand/brand_basic/validation__addDoc.js
@@ -1,8 +1,7 @@
 function( d )
 {
 	print( "-- [ S ] - brand_basic$validation__addDoc():{Object}----------" );
-	// 어떤것을 체크 해야 하나.;
+	// 어떤것을 체크 해야 하나.
 	print( "-- [ E ] - brand_basic$validation__addDoc():{Object}----------" );
-
 	return d;
 }
