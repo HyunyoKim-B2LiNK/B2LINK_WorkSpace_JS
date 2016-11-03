9db56f03128d30a871401b07e6688d5b3a4b21b3
thdtjsdn <thdtjsdn@gmail.com>
2016-11-03 11:42:23 +0900

    [ WebPage ] - U - window.b2link.STATIC - 소스정리
    [ WebPage ] - U - window.b2link.STATIC - 소스정리

--- WebPage/root/js/b2link/__define/101_define_window.b2link.STATIC.CONST.js
+++ WebPage/root/js/b2link/__define/101_define_window.b2link.STATIC.CONST.js
@@ -3,38 +3,44 @@ var fileNm = "js/b2link/__define/101_define_window.b2link.STATIC.CONST.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
-var $d = window.document;
+var $w = window;
+var $d = $w.document;
 
 var _tmp = "";
 
 //--------------------------------------------------[ S ] - window.b2link.STATIC.CONST;
-window.b2link.STATIC.CONST = {};
+
+$w.b2link.STATIC.CONST = {};
 
 
 
 
 
-console.log( "--------------------------------------------------[ S ] - window.b2link.STATIC.CONST.ROOT_DIV" );
-window.b2link.STATIC.CONST.ROOT_DIV = {}
-window.b2link.STATIC.CONST.ROOT_DIV.ID_BACKGROUND = "div_0__Background";
-window.b2link.STATIC.CONST.ROOT_DIV.ID_APPLICATION = "div_0__Application";
-window.b2link.STATIC.CONST.ROOT_DIV.ID_UI_COMPONENT_FIXED = "div_0__UI_Component_Fixed";
-window.b2link.STATIC.CONST.ROOT_DIV.ID_UI_COMPONENT_PANEL = "div_0__UI_Component_Panel";
-window.b2link.STATIC.CONST.ROOT_DIV.ID_TOP = "div_0__Top";
-window.b2link.STATIC.CONST.ROOT_DIV.ID_TOP_MODAL = "div_0__Top_modal";
+window.TtwLog.timeStamp( "--------------------------------------------------[ S ] - window.b2link.STATIC.CONST.ROOT_DIV" );
+$w.b2link.STATIC.CONST.ROOT_DIV = {};
+$w.b2link.STATIC.CONST.ROOT_DIV.ID_BACKGROUND = "div_0__Background";
+$w.b2link.STATIC.CONST.ROOT_DIV.ID_APPLICATION = "div_0__Application";
+$w.b2link.STATIC.CONST.ROOT_DIV.ID_UI_COMPONENT_FIXED = "div_0__UI_Component_Fixed";
+$w.b2link.STATIC.CONST.ROOT_DIV.ID_UI_COMPONENT_PANEL = "div_0__UI_Component_Panel";
+$w.b2link.STATIC.CONST.ROOT_DIV.ID_TOP = "div_0__Top";
+$w.b2link.STATIC.CONST.ROOT_DIV.ID_TOP_MODAL = "div_0__Top_modal";
 
-window.b2link.STATIC.CONST.ROOT_DIV.EL_BACKGROUND = $d.getElementById( "div_0__Background" );
-window.b2link.STATIC.CONST.ROOT_DIV.EL_APPLICATION = $d.getElementById( "div_0__Application" );
-window.b2link.STATIC.CONST.ROOT_DIV.EL_UI_COMPONENT_FIXED = $d.getElementById( "div_0__UI_Component_Fixed" );
-window.b2link.STATIC.CONST.ROOT_DIV.EL_UI_COMPONENT_PANEL = $d.getElementById( "div_0__UI_Component_Panel" );
-window.b2link.STATIC.CONST.ROOT_DIV.EL_TOP = $d.getElementById( "div_0__Top" );
-window.b2link.STATIC.CONST.ROOT_DIV.EL_TOP_MODAL = $d.getElementById( "div_0__Top_modal" );
-console.log( "--------------------------------------------------[ E ] - window.b2link.STATIC.CONST.ROOT_DIV" );
+$w.b2link.STATIC.CONST.ROOT_DIV.EL_BACKGROUND = $d.getElementById( "div_0__Background" );
+$w.b2link.STATIC.CONST.ROOT_DIV.EL_APPLICATION = $d.getElementById( "div_0__Application" );
+$w.b2link.STATIC.CONST.ROOT_DIV.EL_UI_COMPONENT_FIXED = $d.getElementById( "div_0__UI_Component_Fixed" );
+$w.b2link.STATIC.CONST.ROOT_DIV.EL_UI_COMPONENT_PANEL = $d.getElementById( "div_0__UI_Component_Panel" );
+$w.b2link.STATIC.CONST.ROOT_DIV.EL_TOP = $d.getElementById( "div_0__Top" );
+$w.b2link.STATIC.CONST.ROOT_DIV.EL_TOP_MODAL = $d.getElementById( "div_0__Top_modal" );
+window.TtwLog.timeStamp( "--------------------------------------------------[ E ] - window.b2link.STATIC.CONST.ROOT_DIV" );
 
-console.log( "--------------------------------------------------[ S ] - window.b2link.STATIC.CONST.DEVELOPERS" );
-window.b2link.STATIC.CONST.DEVELOPERS = {}
-window.b2link.STATIC.CONST.DEVELOPERS.SSW = "_ssw";
-console.log( "--------------------------------------------------[ E ] - window.b2link.STATIC.CONST.DEVELOPERS" );
+
+
+
+
+window.TtwLog.timeStamp( "--------------------------------------------------[ S ] - window.b2link.STATIC.CONST.DEVELOPERS" );
+$w.b2link.STATIC.CONST.DEVELOPERS = {}
+$w.b2link.STATIC.CONST.DEVELOPERS.SSW = "_ssw";
+window.TtwLog.timeStamp( "--------------------------------------------------[ E ] - window.b2link.STATIC.CONST.DEVELOPERS" );
 
 //--------------------------------------------------[ E ] - window.b2link.STATIC.CONST;
 
