1ebaf50924fc626a307bf38535917e43160c93e8
thdtjsdn <thdtjsdn@gmail.com>
2016-11-03 20:25:33 +0900

    [ WebPage ] - U - URL - 소스 정리
    [ WebPage ] - U - URL - 소스 정리

--- WebPage/root/js/b2link_url/brand/window.b2link_url.brand.brand_basic$getParam.js
+++ WebPage/root/js/b2link_url/brand/window.b2link_url.brand.brand_basic$getParam.js
@@ -1,6 +1,6 @@
 
 //----------------------------------------------------------------------------------------------------;
-var fileNm = "js/b2link_url/window.b2link_url.brand.brand_basic$getParam.js";
+var fileNm = "js/b2link_url/brand/window.b2link_url.brand.brand_basic$getParam.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
@@ -11,16 +11,16 @@ if( console ) console.log( "[ S ] - " + fileNm + "----------" );
  */
 window.b2link_url.brand.brand_basic$getParam = function( d )
 {
-	//window.TtwLog.timeStamp( "---- [ S ] - window.b2link_url.list_static.email_detail$getParam():{String}----------" );
-	//window.TtwLog.timeStamp( "---- [ E ] - window.b2link_url.list_static.email_detail$getParam():{String}----------" );
+	//window.TtwLog.timeStamp( "---- [ S ] - window.b2link_url.brand.brand_basic$getParam():{String}----------" );
+	//window.TtwLog.timeStamp( "---- [ E ] - window.b2link_url.brand.brand_basic$getParam():{String}----------" );
 
-	return "&nm_cn=" + d.nm_cn 
-			+ "&nm_kr=" + d.nm_kr
-			+ "&nm_us=" + d.nm_us
-			+ "&description=" + d.description
-			+ "&url="  + d.url
-			+ "&url_logo" + d.url_logo
-			+ "&url_trademark" + d.url_trademark
+	return "&nm_cn=" + d.nm_cn
+		+ "&nm_kr=" + d.nm_kr
+		+ "&nm_us=" + d.nm_us
+		+ "&description=" + d.description
+		+ "&url="  + d.url
+		+ "&url_logo" + d.url_logo
+		+ "&url_trademark" + d.url_trademark;
 };
 
 //----------------------------------------------------------------------------------------------------;
--- WebPage/root/js/b2link_url/brand/window.b2link_url.brand.brand_basic$getParam_with_brand_id.js
+++ WebPage/root/js/b2link_url/brand/window.b2link_url.brand.brand_basic$getParam_with_brand_id.js
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------------------;
-var fileNm = "js/b2link_url/window.b2link_url.brand.brand_basic$getParam_with_brand_id.js";
+var fileNm = "js/b2link_url/brand/window.b2link_url.brand.brand_basic$getParam_with_brand_id.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
@@ -10,8 +10,8 @@ if( console ) console.log( "[ S ] - " + fileNm + "----------" );
  */
 window.b2link_url.brand.brand_basic$getParam_with_brand_id = function( d )
 {
-	//window.TtwLog.timeStamp( "---- [ S ] - window.b2link_url.list_static.email_detail$getParam():{String}----------" );
-	//window.TtwLog.timeStamp( "---- [ E ] - window.b2link_url.list_static.email_detail$getParam():{String}----------" );
+	//window.TtwLog.timeStamp( "---- [ S ] - window.b2link_url.brand.brand_basic$getParam_with_brand_id():{String}----------" );
+	//window.TtwLog.timeStamp( "---- [ E ] - window.b2link_url.brand.brand_basic$getParam_with_brand_id():{String}----------" );
 
 	return "&_id=" + d._id
 		+ "&nm_cn=" + d.nm_cn
