0c0b29fd18348030ee11c7c4d60871351915515f
thdtjsdn <thdtjsdn@gmail.com>
2016-11-03 17:49:27 +0900

    [ WebPage ] - U - UI - email_detail.update.js 틀린 주석, 로그 정리
    [ WebPage ] - U - UI - email_detail.update.js 틀린 주석, 로그 정리

--- WebPage/root/js/b2link_url/brand/window.b2link_url.brand.brand_basic$getParam.js
+++ WebPage/root/js/b2link_url/brand/window.b2link_url.brand.brand_basic$getParam.js
@@ -6,21 +6,21 @@ if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 
 /**
  * @function
- * @param {Object} d email_detail {}
+ * @param {Object} d
  * @return {String}
  */
 window.b2link_url.brand.brand_basic$getParam = function( d )
 {
-	//window.TtwLog.timeStamp( "---- [ S ] - window.b2link_url.list_static.email_detail$getParam():{String}----------" );
-	//window.TtwLog.timeStamp( "---- [ E ] - window.b2link_url.list_static.email_detail$getParam():{String}----------" );
+	//window.TtwLog.timeStamp( "---- [ S ] - window.b2link_url.brand.brand_basic$getParam():{String}----------" );
+	//window.TtwLog.timeStamp( "---- [ E ] - window.b2link_url.brand.brand_basic$getParam():{String}----------" );
 
-	return "&nm_cn=" + d.nm_cn 
-			+ "&nm_kr=" + d.nm_kr
-			+ "&nm_us=" + d.nm_us
-			+ "&description=" + d.description
-			+ "&url="  + d.url
-			+ "&url_logo=" + d.url_logo
-			+ "&url_trademark=" + d.url_trademark
+	return "&nm_cn=" + d.nm_cn
+		+ "&nm_kr=" + d.nm_kr
+		+ "&nm_us=" + d.nm_us
+		+ "&description=" + d.description
+		+ "&url="  + d.url
+		+ "&url_logo=" + d.url_logo
+		+ "&url_trademark=" + d.url_trademark;
 };
 
 //----------------------------------------------------------------------------------------------------;
