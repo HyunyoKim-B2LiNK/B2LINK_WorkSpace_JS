befbe189423c9010b8599ae046876650ee40c804
ray park <raybirdy@gmail.com>
2016-11-03 15:22:00 +0900

    [ HTTPServer_MongoDB ] - U - brand - brand_basic -update 추가
    id  ids 추가

---
+++ HTTPServer_MongoDB/MemberSession/js/b2link_router/brand/brand_basic/update_ids.js
@@ -0,0 +1,20 @@
+//----------------------------------------------------------------------------------------------------;
+var fileNm = "/js/b2link_router/brand/brand_basic/update_ids.js";
+if( console ) console.log( "[ S ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
+
+/*/
+http://localhost:49320/brand/brand_basic/update_ids
+//*/
+(function( req, res ){
+	global.RayLog.log( "- [ S ] - brand/brand_basic/update_ids():void----------" );
+
+	var q = global.b2link.url.getQueryFromURL( req.url );//Query;
+
+	var d = {};
+	global.b2link_validation.brand.brand_basic$update_ids( d, q );
+
+	global.b2link_router.brand.req__json( req, res, q, "brand_basic$update_ids", d.ids );
+
+	global.RayLog.log( "- [ E ] - email_detail/update_ids():void----------" );
+});
