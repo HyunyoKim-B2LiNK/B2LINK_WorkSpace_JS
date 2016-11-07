f31c9df85b9d052758c37d7d75ba537bef809494
ray park <raybirdy@gmail.com>
2016-11-07 16:55:36 +0900

    [ HTTPServer_MongoDB ] - N - product - product_basic - add 추가

---
+++ HTTPServer_MongoDB/MemberSession/js/b2link_router/product/product_basic/add.js
@@ -0,0 +1,20 @@
+//----------------------------------------------------------------------------------------------------;
+var fileNm = "/js/b2link_router/brand/product_basic/add.js";
+if( console ) console.log( "[ S ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
+
+/*/
+http://localhost:49320/product/product_basic/add
+//*/
+(function( req, res ){
+    global.RayLog.log( "- [ S ] - product/product_basic/add():void----------" );
+
+	var q = global.b2link.url.getQueryFromURL( req.url );//Query;
+    // validation
+	global.b2link_validation.product.product_basic$addDoc( q );
+	// member id from session 
+	// brand id
+	global.b2link_router.product.req__json( req, res, q, "product_basic$addDoc", q );
+
+	global.RayLog.log( "- [ E ] - product/product_basic/add():void----------" );
+});
