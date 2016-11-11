2304b90a5f028571173a6d9a11b9ec01c3dd85cf
thdtjsdn <thdtjsdn@gmail.com>
2016-11-11 11:54:45 +0900

    [ Database Schema ] - U - member.member_basic - getDocProperty__Name 오타 수정
    [ Database Schema ] - U - member.member_basic - getDocProperty__Name 오타
    수정

--- DataBase_Schema/B2LINK/database/member/member_basic/getDocProperty__Name.js
+++ DataBase_Schema/B2LINK/database/member/member_basic/getDocProperty__Name.js
@@ -1,7 +1,7 @@
 function( sid )
 {
 	print( "-- [ S ] - member_basic$getDocProperty__Name():{String}----------" );
-	var o = member_basic$fineOne__sid( sid );
+	var o = member_basic$findOne__sid( sid );
 	print( "-- [ E ] - member_basic$getDocProperty__Name():{String}----------" );
 	return o.nm.a;
 }
