31144901deec398985c2589f1114df580c6f2f63
thdtjsdn <thdtjsdn@gmail.com>
2016-11-11 15:23:28 +0900

    [ WebPage ] - N - b2link_service_member.member_basic - getDocProperty__Name_** 서비스 추가
    [ WebPage ] - N - b2link_service_member.member_basic -
    getDocProperty__Name_** 서비스 추가

---
+++ WebPage/root/js/b2link_service_member/member_basic/window.b2link_service_member.member_basic.getDocProperty__Name_CN.js
@@ -0,0 +1,31 @@
+//----------------------------------------------------------------------------------------------------;
+var fileNm = "js/b2link_service_member/member_basic/window.b2link_service_member.member_basic.getDocProperty__Name_CN.js";
+if ( console ) console.log( "[ S ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
+
+var _t = window.b2link.STATIC.CONFIG.URL;
+var _url = _t.PROXY + _t.MONGODB.MEMBER.BASIC + "getDocProperty__Name_CN?";
+
+/**
+ * @function
+ * @param {*} result
+ * @param {Function} cbFunction function( data ){};
+ */
+//var _fn_res = function( result, cbFunction ){};
+
+/**
+ * @function
+ * @param {Function} cbFunction function( data ){};
+ */
+window.b2link_service_member.member_basic.getDocProperty__Name_CN = function( cbFunction )
+{
+	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_service_member.member_basic.getDocProperty__Name_CN():void----------" );
+
+	window.b2link_service.common._fn_req( _url, cbFunction );
+
+	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_service_member.member_basic.getDocProperty__Name_CN():void----------" );
+};
+
+//----------------------------------------------------------------------------------------------------;
+if( console ) console.log( "[ E ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
