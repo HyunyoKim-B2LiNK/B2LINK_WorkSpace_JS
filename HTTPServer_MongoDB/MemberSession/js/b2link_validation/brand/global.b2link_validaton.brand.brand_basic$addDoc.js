acaa6d9de5392d19486308ef270b84b6e621d22d
ray park <raybirdy@gmail.com>
2016-11-14 13:54:59 +0900

    [ HTTPServer_MongoDB ] - U - brand - brand_basic - addDoc
    validation 모듈 수정

--- HTTPServer_MongoDB/MemberSession/js/b2link_validation/brand/global.b2link_validaton.brand.brand_basic$addDoc.js
+++ HTTPServer_MongoDB/MemberSession/js/b2link_validation/brand/global.b2link_validaton.brand.brand_basic$addDoc.js
@@ -27,16 +27,15 @@ global.b2link_validation.brand.brand_basic$addDoc = function( t, d )
 	}
 	
          
-	d.nm_cn  ? t.nm_cn = d.nm_cn : t.nm_cn = "";
-	d.nm_kr  ? t.nm_kr = d.nm_kr : t.nm_kr = "";	
-	d.nm_us  ? t.nm_us = d.nm_us : t.nm_us = "";
-	d.url ? t.url = d.url : t.url = "";
-	d.url_log ? t.url_logo = d.url_logo : t.url_logo = "";
-	d.url_trademark ? t.url_trademark = d.url_trademark : t.url_trademark = "";
-	d.description ? t.description = d.description : t.description = "";
-	var _date = new Date();
-	t.date_register = [ _date.getFullYear() , _date.getMonth(), _date.getDay(), _date.getHours(), _date.getMinutes(), _date.getSeconds() ];
-	_date = null;
+	t.nm_cn = d.nm_cn ? d.nm_cn : "";
+	t.nm_kr = d.nm_kr ? d.nm_kr : "";	
+	t.nm_us = d.nm_us ? d.nm_us : "";
+	t.url = d.url ?  d.url : "";
+	t.url_logo = d.url_log ? d.url_logo : "";
+	t.url_trademark = d.url_trademark ? d.url_trademark : "";
+	t.description = d.description ? d.description : "";
+	
+	t.date_register = global.b2link.date.getDateArrayFromDate();
 
 	global.RayLog.log( "---- [ E ] - global.b2link_validation.brand.brand_basic$addDoc():{Object}----------" );
 };
