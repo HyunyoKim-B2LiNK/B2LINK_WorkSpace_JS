ab459e158ebef2d1406f5db4090f66ea3f280f77
thdtjsdn <thdtjsdn@gmail.com>
2016-11-15 11:06:27 +0900

    [ HTTPServer_MongoDB ] - U - 소스 정리
    [ HTTPServer_MongoDB ] - U - 소스 정리 - 마지막 공백 라인 제거

--- HTTPServer_MongoDB/MemberSession/js/b2link_router/member/member_basic/getDocProperty__Name.js
+++ HTTPServer_MongoDB/MemberSession/js/b2link_router/member/member_basic/getDocProperty__Name.js
@@ -10,7 +10,6 @@ http://localhost:49320/member/member_basic/getDocProperty__Name
 	global.TtwLog.log( "- [ S ] - member/member_basic/getDocProperty__Name():void----------" );
 
 	var q = global.b2link.url.getQueryFromURL( req.url );//Query;
-		//console.log( "q.sid : " + q.sid );
 
 	global.b2link_router.member.req__string( req, res, q, "member_basic$getDocProperty__Name", q.sid );
 
