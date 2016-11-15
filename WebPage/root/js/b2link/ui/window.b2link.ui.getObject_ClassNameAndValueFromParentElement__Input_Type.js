440fe8f0a4f61b8ce0da91bf38803b63f889deb0
thdtjsdn <thdtjsdn@gmail.com>
2016-11-15 10:47:20 +0900

    [ WebPage ] - U - b2link.ui - API 추가 INPUT VALUE 가져오는 공통 함수 사용하게 변경
    [ WebPage ] - U - b2link.ui - API 추가 INPUT VALUE 가져오는 공통 함수 사용하게 변경

--- WebPage/root/js/b2link/ui/window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type.js
+++ WebPage/root/js/b2link/ui/window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type.js
@@ -12,7 +12,7 @@ window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type = func
 {
 	window.TtwLog.timeStamp( "---- [ S ] - window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type():{Object}----------" );
 
-	var of = window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type;
+	var of = window.b2link.el_input.setObjectInputValueFromInputTypeAndClassName;
 	var r = {};
 	var a = t.children;
 	var io;
@@ -20,18 +20,13 @@ window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type = func
 	for( ; i<iLen; ++i )
 	{
 		io = a[ i ];
-		if( "INPUT" == io.tagName ) of[ "_fn_" + io.type ]( r, io );
+		if( "INPUT" == io.tagName ) of( r, io );
 	}
 
 	window.TtwLog.timeStamp( "---- [ E ] - window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type():{Object}----------" );
 
 	return r;
 };
-window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type._fn_checkbox = function( t, o ){ t[ o.className ] = o.checked; };
-window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type._fn_email = function( t, o ){ t[ o.className ] = o.value; };
-window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type._fn_number = function( t, o ){ t[ o.className ] = o.value; };
-window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type._fn_text = function( t, o ){ t[ o.className ] = o.value; };
-window.b2link.ui.getObject_ClassNameAndValueFromParentElement__Input_Type._fn_url = function( t, o ){ t[ o.className ] = o.value; };
 
 //----------------------------------------------------------------------------------------------------;
 if( console ) console.log( "[ E ] - " + fileNm + "----------" );
