410d445c6dfa3a6284469697c6e2c97de9fc6195
thdtjsdn <thdtjsdn@gmail.com>
2016-11-16 14:56:11 +0900

    [ Databasc Schema ] - D - member.member_basic - 사용 금지
    [ Databasc Schema ] - D - member.member_basic - 사용 금지

--- DataBase_Schema/B2LINK/database/member/member_basic/getDocProperty__Name_CN.js
+++
@@ -1,7 +0,0 @@
-function( sid )
-{
-	print( "-- [ S ] - member_basic$getDocProperty__Name_CN():{String}----------" );
-	var o = member_basic$findOne__sid( sid );
-	print( "-- [ E ] - member_basic$getDocProperty__Name_CN():{String}----------" );
-	return o.nm_cn.a;
-}
