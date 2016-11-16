23590a91127f1b7d215c1dc1abf18c30fead3f79
thdtjsdn <thdtjsdn@gmail.com>
2016-11-16 14:30:10 +0900

    [ HTTPServer_MongoDB ] - N - member.member_public - Service  추가
    [ HTTPServer_MongoDB ] - N - member.member_public - Service  추가

---
+++ HTTPServer_MongoDB/MemberSession/js/b2link_router/member/member_public/getAllList__nm_kr.js
@@ -0,0 +1,17 @@
+//----------------------------------------------------------------------------------------------------;
+var fileNm = "/js/b2link_router/member/member_public/getAllList__nm_kr.js";
+if( console ) console.log( "[ S ] - " + fileNm + "----------" );
+//----------------------------------------------------------------------------------------------------;
+
+/*/
+http://localhost:49320/member_public/getAllList__nm_kr
+//*/
+(function( req, res ){
+	global.TtwLog.log( "- [ S ] - member/member_public/getAllList__nm_kr():void----------" );
+
+	var q = global.b2link.url.getQueryFromURL( req.url );//Query;
+
+	global.b2link_router.member.req__fNm( req, res, q, "member_public$getAllList__nm_kr" );
+
+	global.TtwLog.log( "- [ E ] - member/member_public/getAllList__nm_kr():void----------" );
+});
