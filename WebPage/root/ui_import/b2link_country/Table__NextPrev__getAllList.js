74a93433ef8c6f96684923f4220d31b6b30bd3e1
thdtjsdn <thdtjsdn@gmail.com>
2016-11-16 11:07:31 +0900

    [ WebPage ] - U - list_static.country - Rolling Table 업데이트
    [ WebPage ] - U - list_static.country - Rolling Table 업데이트

--- WebPage/root/ui_import/b2link_country/Table__NextPrev__getAllList.js
+++ WebPage/root/ui_import/b2link_country/Table__NextPrev__getAllList.js
@@ -7,7 +7,7 @@ if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 
 	//window.b2link.ui_resource.apply_LocaleLabel( url, $el_table );
 	//window.b2link.ui_resource.applyParentheses_LocaleLabel( url, $el_table );
-	window.b2link.ui_resource.applyParentheses_LocaleLabel_Children( url, $el_table );
+	//window.b2link.ui_resource.applyParentheses_LocaleLabel_Children( url, $el_table );
 
 	var _this = window.b2linkUIClass.Table.NextPrev__getAllList( url, $el_table );
 		console.log( _this );
