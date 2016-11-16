0d232d5495e35c891786b730d3114bf70fcdafda
thdtjsdn <thdtjsdn@gmail.com>
2016-11-16 19:06:20 +0900

    [ WebPage ] - N - member.member_public - sid로 Member Name 가져오기 추가
    [ WebPage ] - N - member.member_public - sid로 Member Name 가져오기 추가

--- WebPage/root/js/b2link_service_member/member_public/window.b2link_service_member.member_public.getProp_NameFrom_idFromsid.js
+++ WebPage/root/js/b2link_service_member/member_public/window.b2link_service_member.member_public.getProp_NameFrom_idFromsid.js
@@ -1,10 +1,7 @@
 //----------------------------------------------------------------------------------------------------;
-var fileNm = "js/b2link_service_member/member_public/window.b2link_service_member.member_public.getAllList.js";
+var fileNm = "js/b2link_service_member/member_public/window.b2link_service_member.member_public.getProp_NameFrom_idFromsid.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
-
-var _t = window.b2link.STATIC.CONFIG.URL;
-var _url = _t.PROXY + _t.MONGODB.MEMBER.PUBLIC + "getAllList?";
 
 /**
  * @function
@@ -15,15 +12,18 @@ var _url = _t.PROXY + _t.MONGODB.MEMBER.PUBLIC + "getAllList?";
 
 /**
  * @function
+ * @param {uint} _id
  * @param {Function} cbFunction function( data ){};
  */
-window.b2link_service_member.member_public.getAllList = function( cbFunction )
+window.b2link_service_member.member_public.getProp_NameFrom_idFromsid = function( cbFunction )
 {
-	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_service_member.member_public.getAllList():void----------" );
+	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_service_member.member_public.getProp_NameFrom_idFromsid():void----------" );
 
-	window.b2link_service.common._fn_req( _url, cbFunction );
+	window.b2link_service_member.member_session.getProp__idFromsid( function( _id ){
+		window.b2link_service_member.member_public.getProp_NameFrom_id( _id, cbFunction );
+	});
 
-	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_service_member.member_public.getAllList():void----------" );
+	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_service_member.member_public.getProp_NameFrom_idFromsid():void----------" );
 };
 
 //----------------------------------------------------------------------------------------------------;
--- WebPage/root/js/b2link_service_member/member_public/window.b2link_service_member.member_public.getProp_NameFromsid.js
+++ WebPage/root/js/b2link_service_member/member_public/window.b2link_service_member.member_public.getProp_NameFromsid.js
@@ -1,10 +1,10 @@
 //----------------------------------------------------------------------------------------------------;
-var fileNm = "js/b2link_service_member/member_public/window.b2link_service_member.member_public.getAllList.js";
+var fileNm = "js/b2link_service_member/member_public/window.b2link_service_member.member_public.getProp_NameFromsid.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
 var _t = window.b2link.STATIC.CONFIG.URL;
-var _url = _t.PROXY + _t.MONGODB.MEMBER.PUBLIC + "getAllList?";
+var _url = _t.PROXY + _t.MONGODB.MEMBER.PUBLIC + "getProp_NameFromsid?";
 
 /**
  * @function
@@ -15,15 +15,16 @@ var _url = _t.PROXY + _t.MONGODB.MEMBER.PUBLIC + "getAllList?";
 
 /**
  * @function
+ * @param {uint} _id
  * @param {Function} cbFunction function( data ){};
  */
-window.b2link_service_member.member_public.getAllList = function( cbFunction )
+window.b2link_service_member.member_public.getProp_NameFromsid = function( cbFunction )
 {
-	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_service_member.member_public.getAllList():void----------" );
+	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_service_member.member_public.getProp_NameFromsid():void----------" );
 
 	window.b2link_service.common._fn_req( _url, cbFunction );
 
-	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_service_member.member_public.getAllList():void----------" );
+	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_service_member.member_public.getProp_NameFromsid():void----------" );
 };
 
 //----------------------------------------------------------------------------------------------------;
