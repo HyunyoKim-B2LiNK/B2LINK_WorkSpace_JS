f864c2f385fc48b3e5d9fc5872b9278e9edf34e9
thdtjsdn <thdtjsdn@gmail.com>
2016-11-16 14:10:01 +0900

    [ Database Schema ] - D - member.member_basic - 사용 금지 프로시저 제거
    [ Database Schema ] - D - member.member_basic - 사용 금지 프로시저 제거

--- DataBase_Schema/B2LINK/database/member/member_basic/getDocProperty__NameFrom_id.js
+++
@@ -1,7 +0,0 @@
-function( _id )
-{
-	print( "-- [ S ] - member_basic$getDocProperty__NameFrom_id():{String}----------" );
-	var o = member_basic$findOne___id( _id );
-	print( "-- [ E ] - member_basic$getDocProperty__NameFrom_id():{String}----------" );
-	return o.nm.a;
-}
