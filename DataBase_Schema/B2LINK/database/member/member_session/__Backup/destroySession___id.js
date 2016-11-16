7de9cd8ed6372b686687421cda9cdcf17af43a4e
thdtjsdn <thdtjsdn@gmail.com>
2016-11-16 19:05:06 +0900

    [ Database Schema ] - D - _Backup 폴더 제거
    [ Database Schema ] - D - _Backup 폴더 제거

--- DataBase_Schema/B2LINK/database/member/member_session/__Backup/destroySession___id.js
+++
@@ -1,6 +0,0 @@
-function( d )
-{
-	print( "-- [ S ] - member_session$destroySession___id():void----------" );
-	return member_session$_destroySession( member_session$_findOne___id( d._id ) );
-	print( "-- [ E ] - member_session$destroySession___id():void----------" );
-}
