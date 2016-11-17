96bbcf709e94cf7b48554a57fa23acc876f50214
thdtjsdn <thdtjsdn@gmail.com>
2016-11-17 12:03:57 +0900

    [ Database Schema ] - U - member - _Schema 재정의
    [ Database Schema ] - U - member - _Schema 재정의

--- DataBase_Schema/B2LINK/database-insert/_schema/member/member_public.js
+++ DataBase_Schema/B2LINK/database-insert/_schema/member/member_public.js
@@ -1,29 +1,136 @@
+//----------------------------------------------------------------------------------------------------;
+
+function getIntArray( a ){
+	var i = 0;
+	a.forEach( function( io ){ a[ i ] = NumberInt( a[ i ] ); ++i; });
+	return a;
+};
+
+//----------------------------------------------------------------------------------------------------;
+
 var d = {
-	"_id" : "member_basic"
+	"_id" : "member_public"
 
-	, "email" : "string_email"//{String|Array};
-	//, "email_id" : "string"//{String};
-	//, "email_domain" : "string"//{String};
+	//이 Collection을 참조하는 Collections;
+	, "dereference" : [
+		"brand_basic"
+		, "public_basic"
+	]
 
-	, "nm" : {
-		"a" : "string"//{String} Full Name;
-		, "f" : "string"//{String} First Name;
-		, "l" : "string"//{String} Last Name;
-	}
-	, "nm_cn" : {
-		"a" : "string"//{String} Full Name - CH;
-		, "f" : "string"//{String} First Name - CH;
-		, "l" : "string"//{String} Last Name - CH;
-	}
-	, "nm_kr" : {
-		"a" : "string"//{String} Full Name - KR;
-		, "f" : "string"//{String} First Name - KR;
-		, "l" : "string"//{String} Last Name - KR;
-	}
-	, "nm_us" : {
-		"a" : "string"//{String} Full Name - US;
-		, "f" : "string"//{String} First Name - US;
-		, "l" : "string"//{String} Last Name - US;
+	//이 Collection이 참조하는 Collections;
+	, "reference" : [
+		"member_session"
+	]
+
+	//--------------------------------------------------;
+
+	//이 Collection의 Properties;
+	, "properties" : {
+		//Unique Key, Primary Key(PK);
+		"_id" : { "description" : "Unique Key, Primary Key(PK)", "exampleValue" : NumberInt( 0 )
+			, "type" : "uint", "length" : NumberInt( 0 )
+			, "minValue" : NumberInt( 0 )
+			, "maxValue" : NumberInt( 999999999 )
+		}
+
+		//Email;
+		, "email" : { "description" : "Email", "exampleValue" : "thdtjsdn@gmail.com"
+			, "type" : "string", "length" : NumberInt( 0 )
+		}
+
+		//Email's ID;
+		//, "email_id" : { "description" : "Email's ID", "exampleValue" : "thdtjsdn"
+		//	, "type" : "string", "length" : NumberInt( 0 )
+		//}
+
+		//Email's Domain;
+		//, "email_domain" : { "description" : "Email's Domain", "exampleValue" : "gmail.com"
+		//	, "type" : "string", "length" : NumberInt( 0 )
+		//}
+
+		//Name(Default);
+		, "nm" : { "description" : "Name(Default)"
+			, "type" : "object"
+			, "properties" : {
+				//All Name(Default);
+				"a" : { "description" : "All Name(Default)", "exampleValue" : "송선우"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+
+				//First Name(Default);
+				, "f" : { "description" : "First Name(Default)", "exampleValue" : "선우"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+
+				//Last Name(Default);
+				, "l" : { "description" : "Last Name(Default)", "exampleValue" : "송"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+			}
+		}
+
+		//Name(CN);
+		, "nm_cn" : { "description" : "Name(CN)"
+			, "type" : "object"
+			, "properties" : {
+				//All Name(CN);
+				"a" : { "description" : "All Name(CN)", "exampleValue" : "宋善雨"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+
+				//First Name(CN);
+				, "f" : { "description" : "First Name(CN)", "exampleValue" : "善雨"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+
+				//Last Name(CN);
+				, "l" : { "description" : "Last Name(CN)", "exampleValue" : "宋"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+			}
+		}
+
+		//Name(KR);
+		, "nm_kr" : { "description" : "Name(KR)"
+			, "type" : "object"
+			, "properties" : {
+				//All Name(KR);
+				"a" : { "description" : "All Name(KR)", "exampleValue" : "송선우"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+
+				//First Name(KR);
+				, "f" : { "description" : "First Name(KR)", "exampleValue" : "선우"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+
+				//Last Name(KR);
+				, "l" : { "description" : "Last Name(KR)", "exampleValue" : "송"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+			}
+		}
+
+		//Name(US);
+		, "nm_us" : { "description" : "Name(US)"
+			, "type" : "object"
+			, "properties" : {
+				//All Name(US);
+				"a" : { "description" : "All Name(US)", "exampleValue" : "SunWoo Sonw"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+
+				//First Name(US);
+				, "f" : { "description" : "First Name(US)", "exampleValue" : "SunWoo"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+
+				//Last Name(US);
+				, "l" : { "description" : "Last Name(US)", "exampleValue" : "Song"
+					, "type" : "string", "length" : NumberInt( 0 )
+				}
+			}
+		}
 	}
 };
 
--- DataBase_Schema/B2LINK/database-insert/_schema/member/member_time.js
+++ DataBase_Schema/B2LINK/database-insert/_schema/member/member_time.js
@@ -1,34 +1,202 @@
+//----------------------------------------------------------------------------------------------------;
+
+function getIntArray( a ){
+	var i = 0;
+	a.forEach( function( io ){ a[ i ] = NumberInt( a[ i ] ); ++i; });
+	return a;
+};
+
+//----------------------------------------------------------------------------------------------------;
+
 var d = {
 	"_id" : "member_time"
 
-	, "d_l_si" : {
-		"y" : "uint"//{uint} date_last_signin_year;
-		, "m" : "uint"//{uint} date_last_signin_month;
-		, "d" : "uint"//{uint} date_last_signin_day;
-		, "ho" : "uint"//{uint} date_last_signin_hour;
-		, "mi" : "uint"//{uint} date_last_signin_minute;
-		, "se" : "uint"//{uint} date_last_signin_second;
-	}
+	//이 Collection을 참조하는 Collections;
+	, "dereference" : [
+		"member_basic"
+		, "member_session"
+		, "member_time"
+	]
 
-	, "d_su" : {
-		"y" : "uint"//{uint} date_signUp_year;
-		, "m" : "uint"//{uint} date_signUp_month;
-		, "d" : "uint"//{uint} date_signUp_day;
-		, "ho" : "uint"//{uint} date_signUp_hour;
-		, "mi" : "uint"//{uint} date_signUp_minute;
-		, "se" : "uint"//{uint} date_signUp_second;
-	}
+	//이 Collection이 참조하는 Collections;
+	, "reference" : [
+	]
 
-	, "d_sec" : {
-		"y" : "uint"//{uint} date_secession_year;
-		, "m" : "uint"//{uint} date_secession_month;
-		, "d" : "uint"//{uint} date_secession_day;
-		, "ho" : "uint"//{uint} date_secession_hour;
-		, "mi" : "uint"//{uint} date_secession_minute;
-		, "se" : "uint"//{uint} date_secession_second;
-	}
+	//--------------------------------------------------;
 
-	, "mid" : "string_email"//{String};
+	//이 Collection의 Properties;
+	, "properties" : {
+		//Unique Key, Primary Key(PK);
+		"_id" : { "description" : "Unique Key, Primary Key(PK)", "exampleValue" : NumberInt( 0 )
+			, "type" : "uint", "length" : NumberInt( 0 )
+			, "minValue" : NumberInt( 0 )
+			, "maxValue" : NumberInt( 999999999 )
+		}
+
+		//Member ID;
+		, "mid" : { "description" : "Member ID", "exampleValue" : "thdtjsdn@gmail.com"
+			, "type" : "string", "length" : NumberInt( 0 )
+		}
+
+		//Date - Last SignIn;
+		, "d_l_si" : { "description" : "Date - Last SignIn"
+			, "type" : "object"
+			, "properties" : {
+				//Date - Year;
+				"y" : { "description" : "Date - Year", "exampleValue" : "2016"
+					, "type" : "string", "length" : NumberInt( 4 )
+					, "minValue" : NumberInt( 1900 )
+					, "maxValue" : NumberInt( 3000 )
+				}
+
+				//Date - Month;
+				, "m" : { "description" : "Date - Month", "exampleValue" : "11"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 12 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ] )
+				}
+
+				//Date - Day;
+				, "d" : { "description" : "Date - Day", "exampleValue" : "12"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 31 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ] )
+				}
+
+				//Date - Hour;
+				, "ho" : { "description" : "Date - Hour", "exampleValue" : "12"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 24 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ] )
+				}
+
+				//Date - Minute;
+				, "mi" : { "description" : "Date - Minute", "exampleValue" : "59"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 0 )
+					, "maxValue" : NumberInt( 59 )
+					, "values" : getIntArray( [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 ] )
+				}
+
+				//Date - Seconds;
+				, "se" : { "description" : "Date - Seconds", "exampleValue" : "53"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 0 )
+					, "maxValue" : NumberInt( 59 )
+					, "values" : getIntArray( [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 ] )
+				}
+			}
+		}
+
+		//Date - SignUp;
+		, "d_su" : { "description" : "Date - SignUp"
+			, "type" : "object"
+			, "properties" : {
+				//Date - Year;
+				"y" : { "description" : "Date - Year", "exampleValue" : "2016"
+					, "type" : "string", "length" : NumberInt( 4 )
+					, "minValue" : NumberInt( 1900 )
+					, "maxValue" : NumberInt( 3000 )
+				}
+
+				//Date - Month;
+				, "m" : { "description" : "Date - Month", "exampleValue" : "11"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 12 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ] )
+				}
+
+				//Date - Day;
+				, "d" : { "description" : "Date - Day", "exampleValue" : "12"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 31 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ] )
+				}
+
+				//Date - Hour;
+				, "ho" : { "description" : "Date - Hour", "exampleValue" : "12"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 24 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ] )
+				}
+
+				//Date - Minute;
+				, "mi" : { "description" : "Date - Minute", "exampleValue" : "59"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 0 )
+					, "maxValue" : NumberInt( 59 )
+					, "values" : getIntArray( [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 ] )
+				}
+
+				//Date - Seconds;
+				, "se" : { "description" : "Date - Seconds", "exampleValue" : "53"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 0 )
+					, "maxValue" : NumberInt( 59 )
+					, "values" : getIntArray( [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 ] )
+				}
+			}
+		}
+
+		//Date - Secession;
+		, "d_sec" : { "description" : "Date - Secession"
+			, "type" : "object"
+			, "properties" : {
+				//Date - Year;
+				"y" : { "description" : "Date - Year", "exampleValue" : "2016"
+					, "type" : "string", "length" : NumberInt( 4 )
+					, "minValue" : NumberInt( 1900 )
+					, "maxValue" : NumberInt( 3000 )
+				}
+
+				//Date - Month;
+				, "m" : { "description" : "Date - Month", "exampleValue" : "11"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 12 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ] )
+				}
+
+				//Date - Day;
+				, "d" : { "description" : "Date - Day", "exampleValue" : "12"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 31 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ] )
+				}
+
+				//Date - Hour;
+				, "ho" : { "description" : "Date - Hour", "exampleValue" : "12"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 1 )
+					, "maxValue" : NumberInt( 24 )
+					, "values" : getIntArray( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ] )
+				}
+
+				//Date - Minute;
+				, "mi" : { "description" : "Date - Minute", "exampleValue" : "59"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 0 )
+					, "maxValue" : NumberInt( 59 )
+					, "values" : getIntArray( [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 ] )
+				}
+
+				//Date - Seconds;
+				, "se" : { "description" : "Date - Seconds", "exampleValue" : "53"
+					, "type" : "string", "length" : NumberInt( 2 )
+					, "minValue" : NumberInt( 0 )
+					, "maxValue" : NumberInt( 59 )
+					, "values" : getIntArray( [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59 ] )
+				}
+			}
+		}
+	}
 };
 
 //----------------------------------------------------------------------------------------------------;
