04fdb74a0629e2204090059ebc048680e69c9e4e
ray park <raybirdy@gmail.com>
2016-11-17 19:18:44 +0900

    [ Database Schema ] - U - DB Schema 정의 변경
    brand_basic, product_basic, product_catalog

--- DataBase_Schema/B2LINK/database-insert/_schema/product/product_basic.js
+++ DataBase_Schema/B2LINK/database-insert/_schema/product/product_basic.js
@@ -1,30 +1,154 @@
 var d = {
 
-	  "_id" : "product_basic"
-	, "_id$member_public" : "uint"//{uint} - , 해당 제품 등록자
-	, "_id$brand_basic" : "uint"//{uint} - _id$brand_basic , 해당 제품의 브랜드 _id
-	, "_id$product_category" : "uint"//{uint} - 제품 카테고리	제품 카탈로그 _ id
+	 "_id" : "product_basic"
+	  //이 Collection을 참조하는 Collections - Database Name And Collection Name;
+  	, "dereference" : [
+  	]
 
-	, "barcode" : "string"//{String} barcode number	실제 바코드 넘버	제품 바코드 번호
+  	//이 Collection을 참조하는 Collections - Collection Name;
+  	, "dereference_collection" : [
+  	]
 
-	, "date_register" : "array"//{Array} - date_register - [ 2016, 10, 27, 12, 20, 30 ];	제품 등록 일자.
+  	//이 Collection을 참조하는 Database;
+  	, "dereference_database" : [
+  	]
 
-	, "description" : "string"//{String} - description;	 제품 명세
+  	//이 Collection이 참조하는 Collections - Database Name And Collection Name;
+  	, "reference" : [
+  		"brand.brand_basic"
+  		, "member.member_public"
+  		, "product.product_category"
+  	]
 
-	, "nm_kr" : "string"//{String} - name_kr;	제품 한글명
-	, "nm_cn" : "string"//{String} - name_cn;	제품 중문명
-	, "nm_us" : "string"//{String} - name_us;	제품 msds(안전물질 data sheet )
+  	//이 Collection이 참조하는 Collections - Collection Name;
+  	, "reference_collection" : [
+  		"brand_basic"
+  		, "member_public"
+  		, "product_category"
+  	]
 
-	, "msds" : "Boolean"//{Boolean} - MSDS 포함 여부
+  	//이 Collection이 참조하는 Database;
+  	, "reference_database" : [
+  		"brand"
+  		, "member"
+  		, "product"
+  	]
 
+  	//--------------------------------------------------;
 
-	, "url" : "string"//{String} - url - 제품 소개 페이지(공식 홈);
-	, "url_file_product" : "string"//{String} - url_product_img - 제품 이미지 ;
-	, "url_file_barcode": "string"//{String} - url_barcode - 바코드 이미지;
-	, "url_file_msds" : "string"//{String} - msds 이미지 파일
+  	//이 Collection의 Properties;
+  	, "properties" : {
+  		//Unique Key, Primary Key(PK);
+  		"_id" : { "description" : "Unique Key, Primary Key(PK)", "exampleValue" : NumberInt(0)
+  			, "type" : "uint", "length" : NumberInt(0)
+  			, "minValue" : NumberInt(0)
+  			, "maxValue" : 999999999
+  		}
 
-	, "unit_weight" : "string"//{String} / mg, ml,
-	, "weight" : "number"//{Number} - 최소 단위
+  		//Brand ID;
+  		, "_id$brand_basic" : { "description" : "Brand ID(FK)", "exampleValue" : NumberInt(0)
+  			, "type" : "uint", "length" : NumberInt(0)
+  		}
+
+  		//Member ID;
+  		, "_id$member_public" : { "description" : "Member ID(FK)", "exampleValue" : NumberInt(0)
+  			, "type" : "uint", "length" : NumberInt(0)
+  		}
+
+  		//Product Category ID
+  		, "_id$product_category" : { "description" : "Product Category ID(FK)", "exampleValue" : NumberInt(0)
+  			, "type" : "uint", "length" : NumberInt(0)
+  		}
+
+  		//Barcode Number
+  		, "barcode" : { "description" : "Barcode Number", "exampleValue" : "123411231231"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Register Date
+  		, "date_register" : { "description" : "Register Date", "exampleValue" : [ 2016, 10, 27, 12, 20, 30 ]
+  			, "type" : "array", "length" : NumberInt(0)
+  		}
+
+  		//Product Description
+  		, "description" : { "description" : "Product Description", "exampleValue" : "비투링크는 아시아 Kbeauty시장에서 가장영향력 있는 IT 회사"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Description(CN)
+  		, "description_cn" : { "description" : "Product Description(CN)", "exampleValue" : "비투링크는 아시아 Kbeauty시장에서 가장영향력 있는 IT 회사"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Description(KR)
+  		, "description_kr" : { "description" : "Product Description(KR)", "exampleValue" : "비투링크는 아시아 Kbeauty시장에서 가장영향력 있는 IT 회사"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Description(US)
+  		, "description_us" : { "description" : "Product Description(US)", "exampleValue" : "비투링크는 아시아 Kbeauty시장에서 가장영향력 있는 IT 회사"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Boolean Has MSDS
+  		, "msds" : { "description" : "Boolean Has MSDS", "exampleValue" : NumberInt(0)
+  			, "type" : "uint", "length" : NumberInt(1), "default" : NumberInt(0)
+  			, "minValue" : NumberInt(0)
+  			, "maxValue" : NumberInt(1)
+  			, "values" : [ NumberInt(0), NumberInt(1) ]
+  			, "valuesMap" : { "0" : NumberInt(0), "1" : NumberInt(1) }
+  		}
+
+  		//Product Name(Default)
+  		, "nm" : { "description" : "Product Name(Default)", "exampleValue" : "비투링크"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Name(CN)
+  		, "nm_cn" : { "description" : "Product Name(CN)", "exampleValue" : ""
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Name(KR)
+  		, "nm_kr" : { "description" : "Product Name(KR)", "exampleValue" : ""
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Name(US)
+  		, "nm_us" : { "description" : "Product Name(US)", "exampleValue" : "B2Link Inc,"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Unit of Weight
+  		, "unit_weight" : { "description" : "Unit of Weight", "exampleValue" : "mg"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Homepage URL
+  		, "url" : { "description" : "Product Hompage URL", "exampleValue" : "http://www.b2link.co.kr"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Barcode Image URL
+  		, "url_file_barcode" : { "description" : "Barcode Image URL", "exampleValue" : "//path/barcode.img"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//MSDS File URL
+  		, "url_file_msds" : { "description" : "MSDS File URL", "exampleValue" : "//path/msds.file"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Image URL
+  		, "url_file_product" : { "description" : "Product Image URL", "exampleValue" : "//path/product.img"
+  			, "type" : "string", "length" : NumberInt(0)
+  		}
+
+  		//Product Weight
+  		, "url_file_product" : { "description" : "Product Weight", "exampleValue" : "0.5"
+  			, "type" : "number", "length" : NumberInt(0)
+  		}
+  	}
 };
 
 //----------------------------------------------------------------------------------------------------;
---
+++ DataBase_Schema/B2LINK/database-insert/_schema/product/product_category.js
@@ -0,0 +1,91 @@
+var d = {
+
+	"_id" : "product_cateogry"
+	//이 Collection을 참조하는 Collections - Database Name And Collection Name;
+	, "dereference" : [
+		"product.product_basic"
+	]
+
+	//이 Collection을 참조하는 Collections - Collection Name;
+	, "dereference_collection" : [
+		"product_basic"
+	]
+
+	//이 Collection을 참조하는 Database;
+	, "dereference_database" : [
+		"product"
+	]
+
+	//이 Collection이 참조하는 Collections - Database Name And Collection Name;
+	, "reference" : [
+	]
+
+	//이 Collection이 참조하는 Collections - Collection Name;
+	, "reference_collection" : [
+	]
+
+	//이 Collection이 참조하는 Database;
+	, "reference_database" : [
+	]
+
+	//--------------------------------------------------;
+
+	//이 Collection의 Properties;
+	, "properties" : {
+		//Unique Key, Primary Key(PK);
+		"_id" : { "description" : "Unique Key, Primary Key(PK)", "exampleValue" : NumberInt(0)
+			, "type" : "uint", "length" : NumberInt(0)
+			, "minValue" : NumberInt(0)
+			, "maxValue" : NumberInt(999999999)
+		}
+
+		//Code of Category // Array로 수정 ?
+		, "cd_category" : { "description" : "Code of Category(unique value) ", "exampleValue" : "100000000000"
+			, "type" : "string", "length" : NumberInt(12)
+		}
+
+		//Product Category description_us
+		, "description" : { "description" : "Product Category Description(Default)", "exampleValue" : "남성을 위한 화장품"
+			, "type" : "string", "length" : NumberInt(0)
+		}
+
+		//Product Category Description(CN)
+		, "description_cn" : { "description" : "Product Category Description(CN)", "exampleValue" : "남성을 위한 화장품"
+			, "type" : "string", "length" : NumberInt(0)
+		}
+
+		//Product Category Description(KR)
+		, "description_kr" : { "description" : "Product Category Description(KR)", "exampleValue" : "남성을 위한 화장품"
+			, "type" : "string", "length" : NumberInt(0)
+		}
+
+		//Product Category Description(US)
+		, "description_us" : { "description" : "Product Category Description(US)", "exampleValue" : "Cosmetic for man"
+			, "type" : "string", "length" : NumberInt(0)
+		}
+
+		//Product Category Name(Default)
+		, "nm" : { "description" : "Product Name(Default)", "exampleValue" : "남자화장품"
+			, "type" : "string", "length" : NumberInt(0)
+		}
+
+		//Product Category Name(CN)
+		, "nm_cn" : { "description" : "Product Name(CN)", "exampleValue" : ""
+			, "type" : "string", "length" : NumberInt(0)
+		}
+
+		//Product Category Name(KR)
+		, "nm_kr" : { "description" : "Product Name(KR)", "exampleValue" : "남자화장품"
+			, "type" : "string", "length" : NumberInt(0)
+		}
+
+		//Product Category Name(US)
+		, "nm_us" : { "description" : "Product Name(US)", "exampleValue" : "For Man"
+			, "type" : "string", "length" : NumberInt(0)
+		}
+	}
+};
+
+//----------------------------------------------------------------------------------------------------;
+
+db.getSiblingDB( "_schema" ).product.insert( d );
