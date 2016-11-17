94cbb8af71fa74b54bbb824245d4984f79598db8
thdtjsdn <thdtjsdn@gmail.com>
2016-11-17 20:09:23 +0900

    [ Database Schema ] - U - member.member_basic, member_time - 프로시저 정리
    [ Database Schema ] - U - member.member_basic, member_time - 프로시저 정리

--- DataBase_Schema/B2LINK/database/member/member_basic/findOne__sid.js
+++ DataBase_Schema/B2LINK/database/member/member_basic/findOne__sid.js
@@ -1,8 +1,11 @@
 function( sid )
 {
 	print( "-- [ S ] - member_basic$findOne__sid():{Object}----------" );
+
 	var o = member_session$findOne__sid( sid );
-		o = member_basic$_findOne___id( o._id );
+	if( o ) o = member_basic$_findOne___id( o._id );
+
 	print( "-- [ E ] - member_basic$findOne__sid():{Object}----------" );
+
 	return o;
 }
--- DataBase_Schema/B2LINK/database/member/member_time/sign_up.js
+++ DataBase_Schema/B2LINK/database/member/member_time/sign_up.js
@@ -1,17 +1,24 @@
 function( d )
 {
-	print( "-- [ S ] - member_time$sign_up():uint----------" );
+	print( "-- [ S ] - member_time$sign_up():{uint}----------" );
+	printjson( d );
+
 	var doc = member_basic$findOne__mid( d.mid );
+		print( "- doc -" );
+		printjson( doc );
+
 	if( doc )
 	{
-		var doc = member_time$findOne__mid( d.mid )
+		var doc = member_time$findOne__mid( d.mid );
 		if( doc )
 		{
-			print( "-- [ E ] - member_time$sign_up():uint----------if( doc )" );
+			print( "-- [ E ] - member_time$sign_up():{uint}----------return 0 - if( doc )" );
 			return 0;
 		}
 		else
 		{
+			print("- doc else d -");
+			printjson( d );
 			member_time$validation__sign_up( d );
 			member_time$addDoc( d );
 			return 1;
@@ -19,11 +26,13 @@ function( d )
 	}
 	else
 	{
+		print("- d -");
+		printjson( d );
 		member_time$validation__sign_up( d );
 		member_time$addDoc( d );
-		print( "-- [ E ] - member_time$sign_up():uint----------return 1;" );
+		print( "-- [ E ] - member_time$sign_up():{uint}----------return 1;" );
 		return 1;
 	}
-	print( "-- [ E ] - member_time$sign_up():uint----------return 0;" );
+	print( "-- [ E ] - member_time$sign_up():{uint}----------return 0;" );
 	return 0;
 }
