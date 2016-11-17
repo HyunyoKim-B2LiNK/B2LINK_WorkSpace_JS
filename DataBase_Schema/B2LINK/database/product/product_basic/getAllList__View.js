685a755d6731890465a1cabcf19e22954205b045
ray park <raybirdy@gmail.com>
2016-11-17 11:52:22 +0900

    [ Database Schema ] - U - Product - product_basic - getAllList__View
    연과 컬렉션 참조값 반영

--- DataBase_Schema/B2LINK/database/product/product_basic/getAllList__View.js
+++ DataBase_Schema/B2LINK/database/product/product_basic/getAllList__View.js
@@ -1,6 +1,6 @@
 function()
 {
-	print( "---------- [ S ] - getAllList_View():{Array} ---------- ");
+	print( "---------- [ S ] - getAllList__View():{Array} ---------- ");
 	var r = [];
 	print("---------- db.getSiblingDB( brand ) ---------- ");
 	var db_brand = db.getSiblingDB( "brand" );
@@ -11,14 +11,13 @@ function()
 	product_basic$_findAll().forEach( function( doc ){
 		//product_basic$_delete__getAllList( doc );
 		// 20161106 박종하 개발하기
-		print()
 		print(doc)
 		nm_brand = db_brand.brand_basic.findOne( { _id : doc._id$brand_basic});
-		doc._id$brand_basic = nm_brand.nm_kr;
+		doc.brand_name = nm_brand.nm_kr; delete doc._id$brand_basic;
 		nm_member = db_member.member_public.findOne({ _id : doc._id$member_public });
-		doc. = nm_member.nm.a;
+		doc.register_name = nm_member.nm.a; delete doc._id$member_public;
 		r.push( doc );
 	});
-	print( "---------- [ E ] - getAllList_View():{Array} ---------- ");
+	print( "---------- [ E ] - getAllList__View():{Array} ---------- ");
 	return r;
 }
