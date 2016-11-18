669024fa2c114253da926b2b1641952804b431b6
thdtjsdn <thdtjsdn@gmail.com>
2016-11-18 18:01:49 +0900

    [ WebPage ] - D, U - 소스 정리
    [ WebPage ] - D, U - 소스 정리

--- WebPage/root/js/b2link/ui_resource/window.b2link.ui_resource.applyBrace_LocaleLabel.js
+++ WebPage/root/js/b2link/ui_resource/window.b2link.ui_resource.applyBrace_LocaleLabel.js
@@ -12,12 +12,7 @@ window.b2link.ui_resource.applyBrace_LocaleLabel = function( url, el )
 {
 	window.TtwLog.timeStamp( "---- [ S ] - window.b2link.ui_resource.applyBrace_LocaleLabel():void----------" );
 
-	/*/
-	var _o_locale_label = window.b2link.ui_resource.reqSync_JSON_LocaleLabel( url );
-	var o = window.b2link.html.apply_html( el, _o_locale_label );
-	/*/
 	window.b2link.html.applyBrace_html( el, window.b2link.ui_resource.reqSync_JSON_LocaleLabel( url ) );
-	//*/
 
 	window.TtwLog.timeStamp( "---- [ E ] - window.b2link.ui_resource.applyBrace_LocaleLabel():void----------" );
 };
--- WebPage/root/js/b2link/ui_resource/window.b2link.ui_resource.applyBrackets_LocaleLabel.js
+++ WebPage/root/js/b2link/ui_resource/window.b2link.ui_resource.applyBrackets_LocaleLabel.js
@@ -12,12 +12,7 @@ window.b2link.ui_resource.applyBrackets_LocaleLabel = function( url, el )
 {
 	window.TtwLog.timeStamp( "---- [ S ] - window.b2link.ui_resource.applyBrackets_LocaleLabel():void----------" );
 
-	/*/
-	var _o_locale_label = window.b2link.ui_resource.reqSync_JSON_LocaleLabel( url );
-	var o = window.b2link.html.apply_html( el, _o_locale_label );
-	/*/
 	window.b2link.html.applyBrackets_html( el, window.b2link.ui_resource.reqSync_JSON_LocaleLabel( url ) );
-	//*/
 
 	window.TtwLog.timeStamp( "---- [ E ] - window.b2link.ui_resource.applyBrackets_LocaleLabel():void----------" );
 };
