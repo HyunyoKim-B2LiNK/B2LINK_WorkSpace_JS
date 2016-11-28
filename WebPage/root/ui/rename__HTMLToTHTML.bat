c75cdc1269fac8fe162fb8bb36697439ef8e660f
thdtjsdn <thdtjsdn@gmail.com>
2016-11-28 16:05:34 +0900

    [ WebPage ] - U - ui_template 와 ui에 thtml 적용 시킴
    [ WebPage ] - U - ui_template 와 ui에 thtml 적용 시킴

---
+++ WebPage/root/ui/rename__HTMLToTHTML.bat
@@ -0,0 +1,30 @@
+cd b2link_brand_basic
+rename *.html *.thtml
+
+cd..
+cd b2link_country
+rename *.html *.thtml
+
+cd..
+cd b2link_email_detail
+rename *.html *.thtml
+
+cd..
+cd b2link_mashup
+rename *.html *.thtml
+
+cd..
+cd b2link_member
+rename *.html *.thtml
+
+cd..
+cd b2link_product_basic
+rename *.html *.thtml
+
+cd..
+cd b2link_product_category
+rename *.html *.thtml
+
+cd..
+cd b2link_session
+rename *.html *.thtml
