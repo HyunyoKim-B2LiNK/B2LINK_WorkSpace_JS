e29caf594530e82c3dbe6625907dd15c12071588
thdtjsdn <thdtjsdn@gmail.com>
2016-11-29 15:21:34 +0900

    [ Database Schema ] - N - bin - database-insert template json 기능 업데이트
    [ Database Schema ] - N - bin - database-insert template json 기능 업데이트

--- DataBase_Schema/B2LINK/bin/database-insert--_schema_include--CreateFile.js
+++ DataBase_Schema/B2LINK/bin/database-insert--_schema_include--CreateFile.js
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------------------;
-var fileNm = "database--_schema--CreateJSON__FromTemplate.js";
+var fileNm = "database-insert--_schema_include--CreateFile.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
@@ -9,21 +9,29 @@ require( "./_$TATIC_CONST_TtwApplicationCommon.js" );
 
 //--------------------------------------------------;
 
-var path_root = "../database/"
+var path_root = "../database-insert/"
 
 var d = {
+	/*/
 	path_schema : path_root + "_schema/"
 	, path_schema_include : path_root + "_schema_include/"
 	, path_schema_template : path_root + "_schema_template/"
 	, extension : ".json"
 	, template_extension : ".tjson"
+	/*/
+	path_schema : path_root + "_schema_include/"
+	, path_schema_include : path_root + "_schema_include/"
+	, path_schema_template : path_root + "_schema_include/"
+	, extension : ".json"
+	, template_extension : ".tjson"
+	//*/
 };
 
 //--------------------------------------------------;
 
 console.log( "path_schema_template : " + d.path_schema_template );
 
-SUtilFsWriteStream_Extension_Dev.create_JSONsFromTemplateJSONs__Folder( d, d.path_schema_template );
+SUtilFsWriteStream_Extension_Dev.create_Files_FromTemplateFiles__Folder( d, d.path_schema_template );
 
 //----------------------------------------------------------------------------------------------------;
 
