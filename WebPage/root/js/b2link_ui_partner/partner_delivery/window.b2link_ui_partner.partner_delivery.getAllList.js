7fe7d3fc597cab00002de875491a4b73c41e02b9
thdtjsdn <thdtjsdn@gmail.com>
2016-11-29 18:25:01 +0900

    [ WebPage ] - N - UI - partner.partner_*** - JS만 추가 됨
    [ WebPage ] - N - UI - partner.partner_*** - JS만 추가 됨

--- WebPage/root/js/b2link_ui_partner/partner_delivery/window.b2link_ui_partner.partner_delivery.getAllList.js
+++ WebPage/root/js/b2link_ui_partner/partner_delivery/window.b2link_ui_partner.partner_delivery.getAllList.js
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------------------;
-var fileNm = "js/b2link_ui_partner/partner_buyer/window.b2link_ui_partner.partner_buyer.getAllList.js";
+var fileNm = "js/b2link_ui_partner/partner_delivery/window.b2link_ui_partner.partner_delivery.getAllList.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
@@ -7,21 +7,21 @@ if( console ) console.log( "[ S ] - " + fileNm + "----------" );
  * @function
  * @return {Array} [ {HTMLElement}, {HTMLElementController} ]
  */
-window.b2link_ui_partner.partner_buyer.getAllList = function()
+window.b2link_ui_partner.partner_delivery.getAllList = function()
 {
-	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_ui_partner.partner_buyer.getAllList():{Array}----------" );
+	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_ui_partner.partner_delivery.getAllList():{Array}----------" );
 	var d = {
-		url : window.b2link.STATIC.CONFIG.URL.UI.B2LINK_PARTNER_BUYER
+		url : window.b2link.STATIC.CONFIG.URL.UI.B2LINK_PARTNER_DELIVERY
 		, nm : "getAllList"
 		, css_url : "./css_ui/"
-		, css_front_nm : "div__partner_buyer-"
-		, key : "partner_buyer$getAllList"
+		, css_front_nm : "div__partner_delivery-"
+		, key : "partner_delivery$getAllList"
 	};
 
 	//var o = window.b2link.ui.add_HTML_JS_CSS( d );
 	var o = window.b2link.ui.add_HTML_JS_CSS__CheckStaticUI( d );
 
-	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_ui_partner.partner_buyer.getAllList():{Array}----------" );
+	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_ui_partner.partner_delivery.getAllList():{Array}----------" );
 
 	return o;
 };
--- WebPage/root/js/b2link_ui_partner/partner_supplier/window.b2link_ui_partner.partner_supplier.remove.js
+++ WebPage/root/js/b2link_ui_partner/partner_supplier/window.b2link_ui_partner.partner_supplier.remove.js
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------------------;
-var fileNm = "js/b2link_ui_partner/partner_buyer/window.b2link_ui_partner.partner_buyer.update.js";
+var fileNm = "js/b2link_ui_partner/partner_supplier/window.b2link_ui_partner.partner_supplier.remove.js";
 if( console ) console.log( "[ S ] - " + fileNm + "----------" );
 //----------------------------------------------------------------------------------------------------;
 
@@ -7,42 +7,42 @@ if( console ) console.log( "[ S ] - " + fileNm + "----------" );
  * @function
  * @return {Array} [ {HTMLElement}, {HTMLElementController} ]
  */
-window.b2link_ui_partner.partner_buyer.update = function()
+window.b2link_ui_partner.partner_supplier.remove = function()
 {
-	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_ui_partner.partner_buyer.update():{Array}----------" );
+	window.TtwLog.timeStamp( "---- [ S ] - window.b2link_ui_partner.partner_supplier.remove():{Array}----------" );
 	var d = {
-		url : window.b2link.STATIC.CONFIG.URL.UI.B2LINK_PARTNER_BUYER
-		, nm : "update"
+		url : window.b2link.STATIC.CONFIG.URL.UI.B2LINK_PARTNER_SUPPLIER
+		, nm : "remove"
 		, css_url : "./css_ui/"
-		, css_front_nm : "div__partner_buyer-"
-		, key : "partner_buyer$update"
+		, css_front_nm : "div__partner_supplier-"
+		, key : "partner_supplier$remove"
 	};
 
 	//var o = window.b2link.ui.add_HTML_JS_CSS( d );
 	var o = window.b2link.ui.add_HTML_JS_CSS__CheckStaticUI( d );
 
-	if( !o[ 1 ].getEvt_Complete__Update() )
+	if( !o[ 1 ].getEvt_Complete__Delete() )
 	{
-		o[ 1 ].setEvt_Complete__Update( function( result )
+		o[ 1 ].setEvt_Complete__Delete( function( result )
 		{
-			window.TtwLog.timeStamp( "[ S ] - o[ 1 ].setEvt_Complete__Update( function( result ){----------" );
+			window.TtwLog.timeStamp( "[ S ] - o[ 1 ].setEvt_Complete__Delete( function( result ){----------" );
 			if( window.b2link.fn.getResultStatus( result ) )
 			{
 				/*/
 				//o[ 1 ].hide();
 				////window.b2link.element.setPosition_RightTop( o[ 0 ], 10, 300 );
-				//window.b2link_ui_partner.partner_buyer.getAllList();
+				//window.b2link_ui_partner.partner_supplier.getAllList();
 				/*/
 				o[ 1 ].req_Data();
 				//*/
 			}
-			else alert( "수정 실패." );
+			else alert( "삭제 실패." );
 
-			window.TtwLog.timeStamp( "[ E ] - o[ 1 ].setEvt_Complete__Update( function( result ){----------" );
+			window.TtwLog.timeStamp( "[ E ] - o[ 1 ].setEvt_Complete__Delete( function( result ){----------" );
 		});
 	}
 
-	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_ui_partner.partner_buyer.update():{Array}----------" );
+	window.TtwLog.timeStamp( "---- [ E ] - window.b2link_ui_partner.partner_supplier.remove():{Array}----------" );
 
 	return o;
 };
